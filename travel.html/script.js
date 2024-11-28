8760af737589889873d28637d337181cc61b27ba
Kishore <116958416+Murugan007Kishore@users.noreply.github.com>
2024-11-28 09:58:58 +0530

    Add files via upload
    TRAVEL AGENCY NAANMUDHALAVAN PROJECT

---
+++ travel.html/script.js
@@ -0,0 +1,6 @@
+// Contact Form Submission
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    document.getElementById('successMessage').style.display = 'block';
+  });
+  
